893bee27b7f14c71d229cdc51e6ec1d0619bfe36
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-01 12:41:30 -0700

    Task 2.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useReducer} from 'react';
 import './App.css';
 import reducer, {initialState} from './reducers/';
+import {addOne} from './actions/';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
@@ -9,6 +10,10 @@ function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log('state: ', state);
+
+  const handleClick = (e) => {
+    dispatch(addOne());
+  }
 
   return (
     <div className="App">
@@ -33,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleClick}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
